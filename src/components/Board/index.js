aba758d7dd3cf80df50a5679c2ab9bcf91ed0f54
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-02-09 14:06:28 +0000

    added reset button

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -9,70 +9,77 @@ import './board.css';
 // work on board design (maybe we can use a card or image for x and o)
 
 function Board(props) {
-	const [board, setBoard] = useState([
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-	]);
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
 
-	const changeBoardSquare = (index, newValue) => {
-		const newBoard = [...board];
-		newBoard[index] = newValue;
-		props.changeTurn();
-		setBoard(newBoard);
-		checkGameStatus();
-	};
-	const checkGameStatus = () => {
-		const winConditions = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
+  const changeBoardSquare = (index, newValue) => {
+    const newBoard = [...board];
+    newBoard[index] = newValue;
+    props.changeTurn();
+    setBoard(newBoard);
+    checkGameStatus();
+  };
+  const checkGameStatus = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-		winConditions.forEach((element, index) => {
-			if (
-				board[element[0]] === board[element[1]] &&
-				board[element[1]] === board[element[2]] &&
-				board[element[0]] !== null
-			) {
-				console.log('game over! winner is ' + board[element[0]]);
-				return;
-			}
-		});
-		const freeSquareId = board.findIndex((el) => el === null);
-		if (freeSquareId === -1) {
-			console.log('Game ends in a draw!');
-		}
-	};
+    winConditions.forEach((element, index) => {
+      if (
+        board[element[0]] === board[element[1]] &&
+        board[element[1]] === board[element[2]] &&
+        board[element[0]] !== null
+      ) {
+        console.log('game over! winner is ' + board[element[0]]);
+        return;
+      }
+    });
+    const freeSquareId = board.findIndex((el) => el === null);
+    if (freeSquareId === -1) {
+      console.log('Game ends in a draw!');
+    }
+  };
 
-	useEffect(checkGameStatus, [checkGameStatus]);
+  useEffect(checkGameStatus, [checkGameStatus]);
 
-	return (
-		<div className='board'>
-			{board.map((el, index) => {
-				return (
-					<Square
-						changeSquare={changeBoardSquare}
-						id={index}
-						key={index}
-						turn={props.turn}
-						status={el}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="board">
+      {board.map((el, index) => {
+        return (
+          <Square
+            changeSquare={changeBoardSquare}
+            id={index}
+            key={index}
+            turn={props.turn}
+            status={el}
+          />
+        );
+      })}
+      <button
+        onClick={() => {
+          setBoard([null, null, null, null, null, null, null, null, null]);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default Board;
