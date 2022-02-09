6b8e72a07470dc18f156c23f3998cecd97f8cab9
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-02-09 12:14:33 +0000

    structure started

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [board,setBoard] = useState([null,null,null,null,null,null,null,null,null])
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
